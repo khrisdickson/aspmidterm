c95a102a2a585b6d9e55e90218fa22cff506810f
khrisdickson <xkhrisx@gmail.com>
2015-07-02 15:47:05 -0400

    https://github.com/khrisdickson/aspmidterm

---
+++ ASP Midterm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_Midterm.Startup))]
+namespace ASP_Midterm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
